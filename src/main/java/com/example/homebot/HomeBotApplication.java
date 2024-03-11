b6875a1767a9879ff62b8fbcc71a5a9138c06888
Vadim Yevstiunin <vadimyevstiunin@gmail.com>
2024-03-11 22:01:30 +0100

    Initial commit

---
+++ src/main/java/com/example/homebot/HomeBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.homebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homebot/HomeBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
